45773ca0d980dd2dfbd0b9abbdcbfbcc56dde913
Steven Delpercio <stevendelpercio1992@hotmail.com>
2020-09-02 09:44:58 -0400

    users displayed in a users component

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import api from "../utils/api";
 
 export const Users = () => {
+  const [usernames, setUsers] = useState([]);
+  useEffect(() => {
+    usersList();
+  }, []);
+
   const usersList = async () => {
     const users = await api.getUsers();
-    console.log(users);
+    console.log(users.users);
+    const usernamesArray = users.users.map((userInfo) => userInfo.username);
+    console.log(usernamesArray);
+    setUsers(usernamesArray);
   };
 
   return (
     <div>
       <h1>Hello</h1>
+      <ul>
+        {usernames.map((profileName) => (
+          <>
+            <li>{profileName}</li>
+          </>
+        ))}
+      </ul>
     </div>
   );
 };
-
