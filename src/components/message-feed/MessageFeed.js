e7558d543473d9c99f784c35c25e76337491369f
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-09-04 15:28:01 -0400

    added button to each button (in development)

--- src/components/message-feed/MessageFeed.js
+++ src/components/message-feed/MessageFeed.js
@@ -39,6 +39,7 @@ export const MessageFeed = () => {
                 <span>UserId: {message[1].username}</span>
                 <span>Likes: {messages[1].likes.length}</span>
                 <span>Posted: {messages[1].createdAt.toString()}</span>
+                <button>Like/Unlike(In development Stage)</button>
               </div>
             </>
           ))}
