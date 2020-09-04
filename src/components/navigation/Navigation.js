58724f3714bc8c7fe2a21b73c2b8daefaf5678ca
zacharybaca <zachbaca@icloud.com>
2020-09-04 14:15:26 -0500

    fixed import bug

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
-import { HomeScreen, ProfileScreen, NotFoundScreen } from "../../screens";
+import {
+  HomeScreen,
+  ProfileScreen,
+  NotFoundScreen,
+  Users,
+} from "../../screens";
 
 import { ConnectedRoute } from "../connected-route/ConnectedRoute";
 import { RegisterForm } from "../register-form";
@@ -23,14 +28,9 @@ export const Navigation = () => (
         path="/profiles/:username"
         component={ProfileScreen}
       />
-      <ConnectedRoute 
-      exact 
-      isProtected
-      path="/users"
-      component ={Users}
-      />
-      <ConnectedRoute path="/register" component={RegisterForm}/>
-      <ConnectedRoute path="/messagefeed" component={MessageFeedContainer}/>
+      <ConnectedRoute exact isProtected path="/users" component={Users} />
+      <ConnectedRoute path="/register" component={RegisterForm} />
+      <ConnectedRoute path="/messagefeed" component={MessageFeedContainer} />
       <ConnectedRoute path="*" component={NotFoundScreen} />
     </Switch>
   </BrowserRouter>
