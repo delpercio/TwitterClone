8f46de339e92770a80172656943ab786063559ac
zacharybaca <zachbaca@icloud.com>
2020-09-07 16:09:24 -0500

    added some css changes

--- src/components/message-feed/MessageFeed.js
+++ src/components/message-feed/MessageFeed.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/messages";
 import { Loader } from "../loader";
 import styled from "styled-components";
-import { MenuContainer } from "../../components"
+import { MenuContainer } from "../../components";
 import "./MessageFeed.css";
 
 const TitleHeader = styled.h2`
@@ -31,26 +31,28 @@ export const MessageFeed = () => {
   const handleMessageArray = () => {
     if (messages) {
       return (
-        <>
-        <MenuContainer/>
+        <div>
+          <div className="menu-container">
+            <MenuContainer />
+          </div>
+
           <TitleHeader>Messages</TitleHeader>
           {messagesArray.map((message) => (
-            <>
-              <div className="messages-info">
-                <li key={message.id}>{message[1].text}</li>
-                <span>UserId: {message[1].username}</span>
-                <span>Likes: {messages[1].likes.length}</span>
-                <span>Posted: {messages[1].createdAt.toString()}</span>
-                <button>Like/Unlike(In development Stage)</button>
-              </div>
-            </>
+            <div className="messages-info">
+              <li key={message.id}>{message[1].text}</li>
+              <span>UserId: {message[1].username}</span>
+              <span>Likes: {messages[1].likes.length}</span>
+              <span>Posted: {messages[1].createdAt.toString()}</span>
+              <button>Like/Unlike(In development Stage)</button>
+            </div>
           ))}
-        </>
+        </div>
       );
     } else {
       return Loader;
     }
   };
+
   return (
     <div className="feed">
       <div className="messages">{handleMessageArray()}</div>
