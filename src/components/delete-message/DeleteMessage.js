1ffbe0658e15f61a4fdf02b2b29c7a0a76dd0750
zacharybaca <zachbaca@icloud.com>
2020-09-09 08:53:19 -0500

    changes

---
+++ src/components/delete-message/DeleteMessage.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+
+export const DeleteMessage (id) => {
+    const [deleteMessage, setDeleteMessage] = useState(id);
+    return (
+        <div>
+        </div>
+    )
+}
