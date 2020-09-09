ead8d19cb11fc8a87244e6b9d712e284f16d4a1f
Steven Delpercio <stevendelpercio1992@hotmail.com>
2020-09-09 10:12:34 -0400

    added 404 header

--- src/redux/reducers/likes.js
+++ src/redux/reducers/likes.js
@@ -1,1 +1,3 @@
 // TODO: implement
+
+// reducer should
--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {MenuContainer} from '../components/menu'
 
 const NotFound = ({ location }) => (
   <>
-    <p>Page not found for {location.pathname}</p>
+  <MenuContainer/>
+    <p>404...Page not found for {location.pathname}</p>
     <Link to="/">Go Home</Link>
   </>
 );
