470f94a89e936b7e9df2d887ad6f42057cc20244
Steven Delpercio <stevendelpercio1992@hotmail.com>
2020-09-09 09:57:42 -0400

    decided to move forward without redux right now on users

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,1 +1,32 @@
 // TODO: implement
+// import { GET_USERS } from "../actions";
+
+// // INITIAL STATE
+// const INITIAL_STATE = {
+//   users:[]
+// };
+
+// const getUsersReducer = (state = { ...INITIAL_STATE }, action) => {
+//   switch (action.type) {
+//     case GET_USERS:
+//         const usersList = async () => {
+//             const users = await api.getUsers();
+//             const usersArr = users.users;
+//           };
+//       return {
+//         ...INITIAL_STATE,usersArr
+//       };
+//     case LOGIN_FAILURE:
+//       return {
+//         ...INITIAL_STATE,
+//         error: action.payload,
+//         loading: false,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default getUsersReducer;
+
+
