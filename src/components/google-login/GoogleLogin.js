c58ac299302ab6bef78c78ee746e65b47becc922
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-09-11 11:51:04 -0400

    Google-login button now works

--- src/components/google-login/GoogleLogin.js
+++ src/components/google-login/GoogleLogin.js
@@ -1,33 +1,30 @@
 import React from 'react'
-import API from "../../utils/api"
-import GoogleLogin from "react-google-login"
-
+import { actions } from "../../redux/actions/auth"
+import { useDispatch } from "react-redux"
 
 
 export function GoogleLoginComponent() {
-    const successfulLogin = (response) => {
-        console.log("successful")
-        console.log({response})
-    }
+    const dispatch = useDispatch()
 
-    const failedLogin = (response) => {
-        console.log("failed")
-        console.log({response})
-    }
-
-    const responseGoogle = (response) => {
-        console.log({response})
+    const responseGoogle = async (event) => {
+        const openerWindow = window.open("https://kwitter-api.herokuapp.com/auth/google/login")
+        openerWindow.window.opener.onmessage = function (event) {
+            console.log(event)
+            if (event.data.statusCode !== 200) {
+                openerWindow.close()
+                return 
+            }else {
+                dispatch(actions.googleLogin(event.data))
+                openerWindow.close()
+            }
+        }
     }
     return (
         <>
 
-        <GoogleLogin
-        clientId="24161483118-bet5ubp3fb8cj7a0s3an404732n0ooke.apps.googleusercontent.com"
-        buttonText="Sign in with Google"
-        onSuccess={successfulLogin}
-        onFailure={failedLogin}
-        cookiePolicy={'single_host_origin'}
-        />
+            <button onClick={responseGoogle}>
+                Sign in with Google
+        </button>
         </>
     )
 }
