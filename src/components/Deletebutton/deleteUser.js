02c26e3a35e35bc5d9f0487e26fb4dc5329b1847
Jeyquan Johnson <fatjey@Jeyquans-MacBook-Air.local>
2020-09-11 13:47:34 -0400

    logout after delete

--- src/components/Deletebutton/deleteUser.js
+++ src/components/Deletebutton/deleteUser.js
@@ -1,42 +1,42 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import API from "../../utils/api";
 import { connect } from "react-redux";
-import { LOGOUT } from "../../redux/actions/auth";
-import { actions } from "../../redux/actions/auth";
+import { actions } from "../../redux/actions/auth"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./deleteUser.css";
-import { Redirect } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+
 
 
 
 const DeleteUser = (props) => {
-    const handleDelete = async () => {
+   const [deleteMessage, setDeleteMessage] = useState(null)
     let txt = "";
+    const handleDelete = async () => {
     if (window.confirm("Are You Sure You Want To Delete?") === true) {
-      txt = "Delete Confirmed!";
+      
       console.log(props)
-     window.confirm("Make Sure To Click Logout After You Delete Profile")
-      await API.deleteUser(props.auth.username);
      
-     console.log(props.auth.username);
-   
-    
-   
-    } else {
+      await API.deleteUser(props.auth.username);
+      setDeleteMessage("User Has Been Deleted Logging Out!")
+      props.logout()
+   } else {
       txt = "Delete Cancelled";
     }
   };
 
-//   console.log(props.auth.username);
+
   return (
     <div>
       <button onClick={handleDelete}>Delete User</button>
+      <h2>{deleteMessage}</h2>
     </div>
   );
 };
+function mapDispatchToProps(dispatch){
+    return {logout:() => dispatch(actions.logout())}
+}
 function mapStateToProps(state) {
   return state;
 }
 
-export default connect(mapStateToProps)(DeleteUser);
+export default connect(mapStateToProps , mapDispatchToProps)(DeleteUser);
